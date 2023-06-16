5a0f5c50e94bb653b35a5e1de30ddf18e2a5e6d9
Jeff VanOss <jeff.vanoss@bamfhealth.com>
2023-06-16 19:12:35 +0000

    convert to slicer seg.nrrd

--- setup.py
+++ setup.py
@@ -51,6 +51,6 @@ setup(name='TotalSegmentator',
         scripts=[
             'bin/TotalSegmentator', 'bin/totalseg_combine_masks', 'bin/crop_to_body', 
             'bin/totalseg_import_weights', 'bin/totalseg_download_weights',
-            'bin/totalseg_setup_manually'
+            'bin/totalseg_setup_manually', 'bin/totalseg_to_slicer_seg'
         ]
     )
